ddcecfb7493e99f67d204ba571794d45758fe5a9
Amoller655 <amoller655@gmail.com>
2018-11-18 17:22:42 -0500

    Created New Robot and RobotLift subsystem

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GMR/Robot/Robot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.GMR.Robot;
+
+import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.DriveTrain;
+import org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems.RobotLift;
+
+public class Robot {
+
+    public DriveTrain driveTrain;
+    public RobotLift robotLift;
+
+    private DcMotor leftFront;
+    private DcMotor leftRear;
+    private DcMotor rightFront;
+    private DcMotor rightRear;
+    private IntegratingGyroscope gyro;
+
+    private DcMotor liftMotor;
+
+    public Robot (HardwareMap hardwareMap, Telemetry telemetry){
+
+        leftFront = hardwareMap.dcMotor.get("leftfront");
+        leftRear = hardwareMap.dcMotor.get("leftrear");
+        rightFront = hardwareMap.dcMotor.get("rightfront");
+        rightRear = hardwareMap.dcMotor.get("rightrear");
+        gyro = hardwareMap.get(NavxMicroNavigationSensor.class, "navx");
+
+        liftMotor = hardwareMap.dcMotor.get("liftmotor");
+
+
+        driveTrain = new DriveTrain(leftFront, rightFront, leftRear, rightRear, gyro, telemetry);
+
+        robotLift = new RobotLift(liftMotor, telemetry);
+    }
+}
