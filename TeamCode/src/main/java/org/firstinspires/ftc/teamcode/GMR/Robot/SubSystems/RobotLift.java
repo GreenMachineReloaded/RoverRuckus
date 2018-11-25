a813681c90851b3bb13a28ad0451215d6ca9e14e
Amoller655 <amoller655@gmail.com>
2018-11-25 16:22:37 -0500

    Added Auto Encoder functionality to Lift
    setLift() uses a range from 0.0 to 1.0 for the goal position rather than actual encoder values for simplicity. The value is then scaled to equate to actual encoder counts.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GMR/Robot/SubSystems/RobotLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GMR/Robot/SubSystems/RobotLift.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.GMR.Robot.SubSystems;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -10,6 +11,8 @@ public class RobotLift {
 
     private Telemetry telemetry;
 
+    private boolean encodersCanRun;
+
     public RobotLift(DcMotor liftMotor, Telemetry telemetry){
         this.liftMotor = liftMotor;
         this.telemetry = telemetry;
@@ -17,6 +20,8 @@ public class RobotLift {
         liftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         liftMotor.setPower(0);
+
+        this.encodersCanRun = true;
     }
 
     public void lift (boolean bumper, float trigger) {
@@ -38,5 +43,34 @@ public class RobotLift {
             liftMotor.setPower(0);
         }
         telemetry.addData("Lift Encoder:", liftMotor.getCurrentPosition());
+    }
+
+    public boolean setLift (double goalPos,  double power) {
+        //Input goalPos must be between 0.0 and 1.0
+
+        int currentPos = liftMotor.getCurrentPosition();
+
+        if(encodersCanRun){
+            //goalPos is scaled out to work with encoder values
+            goalPos = Range.clip(goalPos, 0.0, 1.0);
+            goalPos = Range.scale(goalPos, 0.0, 1.0, 5, 2830);
+            encodersCanRun = false;
+            return encodersCanRun;
+        } else {
+            if(currentPos < goalPos){
+                liftMotor.setPower(-power);
+                telemetry.addData("Current Lift Value:", currentPos);
+                telemetry.addData("Lift Goal Value:", goalPos);
+            } else if(currentPos > goalPos){
+                liftMotor.setPower(power);
+                telemetry.addData("Current Lift Value:", currentPos);
+                telemetry.addData("Lift Goal Value:", goalPos);
+            } else {
+                encodersCanRun = true;
+                liftMotor.setPower(0.0);
+                return encodersCanRun;
+            }
+        }
+        return false;
     }
 }
